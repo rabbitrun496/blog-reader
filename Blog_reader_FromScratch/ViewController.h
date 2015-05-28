f5314b337819613a57bfad8161fc9661762acb88
SHAJI ALI <shajiali@Remotes-MacBook-Air.local>
2015-05-28 18:39:23 -0400

    Initial Commit

---
+++ Blog_reader_FromScratch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blog_reader_FromScratch
+//
+//  Created by SHAJI ALI on 5/28/15.
+//  Copyright (c) 2015 SHAJI ALI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Blog_reader_FromScratch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blog_reader_FromScratch
+//
+//  Created by SHAJI ALI on 5/28/15.
+//  Copyright (c) 2015 SHAJI ALI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
