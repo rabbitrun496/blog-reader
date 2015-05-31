f7eb401d46b4c8caf932a44c815684b884c88cf6
shaji ali <shaji.corp@gmail.com>
2015-05-31 19:45:37 -0400

    initial commit
    initialize the project

---
+++ Blog_reader_FromScratch/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  Blog_reader_FromScratch
+//
+//  Created by SHAJI ALI on 5/29/15.
+//  Copyright (c) 2015 SHAJI ALI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *blogPosts;
+
+
+@end
